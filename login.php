db4e5d97734b353337806738905c5ed324571380
053_MIB_Feis Aulia <70646439+feisauu@users.noreply.github.com>
2023-12-01 10:14:35 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,31 @@
+<?php
+include('includes/header.php');
+include('includes/navbar.php');
+?>
+
+<div class="py-5">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+              <div class="card">
+                <div class="card-header">
+                    <h4>Login</h4>
+                </div>
+                <div class="card-body">
+                    <div class="form-group mb-3">
+                        <label>Email Id</label>
+                        <input type="text" placeholder="Enter Email Address" class="form-control">
+                    </div>
+                    <div class="form-group mb-3">
+                        <label>Password</label>
+                        <input type="text" placeholder="Enter password" class="form-control">
+                    </div>
+                    <div class="form-group mb-3">
+                        <button type="submit" class="btn btn-primary">Login Now</button>
+                    </div>
+                </div>
+              </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ register.php
@@ -0,0 +1,45 @@
+<?php
+include('config/dbconn.php');
+
+include('includes/header.php');
+include('includes/navbar.php');
+?>
+
+<div class="py-5">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+              <div class="card">
+                <div class="card-header">
+                    <h4>Register</h4>
+                </div>
+                <div class="card-body">
+                <div class="form-group mb-3">
+                        <label>First Name</label>
+                        <input type="text" placeholder="Enter First Name" class="form-control">
+                    </div>
+                    <div class="form-group mb-3">
+                        <label>Last Name</label>
+                        <input type="text" placeholder="Enter Last Name" class="form-control">
+                    </div>
+                    <div class="form-group mb-3">
+                        <label>Email Id</label>
+                        <input type="text" placeholder="Enter Email Address" class="form-control">
+                    </div>
+                    <div class="form-group mb-3">
+                        <label>Password</label>
+                        <input type="text" placeholder="Enter password" class="form-control">
+                    </div>
+                    <div class="form-group mb-3">
+                        <button type="submit" class="btn btn-primary">Login Now</button>
+                    </div>
+                </div>
+              </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include('includes/footer.php');
+?>
